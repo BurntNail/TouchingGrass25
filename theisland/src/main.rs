256259e10d72489de3577f1cf317fc5659575234
Jack Maguire <jack@maguire.tech>
2025-10-25 12:19:30 +0100

    backend fun

--- theisland/src/main.rs
+++ theisland/src/main.rs
@@ -1,8 +1,40 @@
+use axum::extract::DefaultBodyLimit;
+use axum::response::Html;
+use axum::Router;
+use axum::routing::{get, post};
+use tokio::net::TcpListener;
+use crate::backend::{get_leaderboard, leaderboard_sse, start_grass, submit_grass};
+use crate::state::IslandState;
+use tower_http::cors::{AllowOrigin, CorsLayer};
+use tracing::info;
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+
 mod state;
-mod routes;
-mod error;
+mod backend;
+mod errors;
 
 #[tokio::main]
 async fn main() {
+    tracing::subscriber::set_global_default(
+        FmtSubscriber::builder()
+            .with_env_filter(EnvFilter::from_default_env())
+            .finish()
+    )
+        .unwrap();
 
+    let state = IslandState::default();
+
+    let app = Router::new()
+        .route("/", get(Html(include_str!("../testingforms.html"))))
+        .route("/start_grass", post(start_grass))
+        .route("/submit_grass", post(submit_grass))
+        .route("/leaderboard", get(get_leaderboard))
+        .route("/leaderboard_sse", get(leaderboard_sse))
+        .layer(CorsLayer::default().allow_origin(AllowOrigin::any()))
+        .layer(DefaultBodyLimit::max(64_000_000))
+        .with_state(state);
+
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    info!(?listener, "listening on");
+    axum::serve(listener, app).await.unwrap();
 }
