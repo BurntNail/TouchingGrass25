38694aa6ba68494ff0f8a1452b545dfb317323df
michalina07 <michalianadymitruk@gmai.com>
2025-10-25 13:35:51 +0100

    green

--- Html/script.js
+++ Html/script.js
@@ -6,24 +6,38 @@ document.addEventListener("DOMContentLoaded", function() {
     from_web.then(response => {
         response.json().then(data => {
             for (let leaderboardEntry of data) {
-                rank = rank + 1;
-                
                 let row = document.createElement("tr");
-
-
+               
                 let rankElement = document.createElement("td");
                 rankElement.innerText = rank;
                 row.appendChild(rankElement);
+                if(rank <= 3){
+                    rankElement.style.color = "LightGreen";
+                }else{
+                    rankElement.style.color = "black";
+                }
 
                 let nameElement = document.createElement("td");
                 nameElement.innerText = leaderboardEntry.person;
                 row.appendChild(nameElement);
+                if(rank <= 3){
+                    nameElement.style.color = "LightGreen";
+                }else{
+                    nameElement.style.color = "black";
+                }
+
 
                 let scoreElement = document.createElement("td");
                 scoreElement.innerText = leaderboardEntry.score;
                 row.appendChild(scoreElement);
+                 if(rank <= 3){
+                    scoreElement.style.color = "LightGreen";
+                }else{
+                    scoreElement.style.color = "black";
+                }
 
                 table.appendChild(row);
+                rank = rank + 1;
             }
         });
     });
