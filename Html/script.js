68f16c9196c95ba58e1328b5c7de6de06a0ed788
Simon Lynch <sl411@st-andrews.ac.uk>
2025-10-25 10:26:00 +0100

    Added external CSS and js

--- Html/script.js
+++ Html/script.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     const table = document.querySelector("table");
     const from_web = fetch('https://theisland.fly.dev/leaderboard');
-    from_web.then(response => table.innerHTML = response.text());
+    from_web.then(response => {
+        response.text().then(data => {
+            table.innerHTML = data;
+        });
+    });
 });
