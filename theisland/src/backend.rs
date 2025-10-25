7301f516a10fde06fdb71e384d9257a452ff24bc
Jack Maguire <jack@maguire.tech>
2025-10-25 15:41:04 +0100

    backend crap

--- theisland/src/backend.rs
+++ theisland/src/backend.rs
@@ -1,13 +1,18 @@
 use crate::errors::IslandError;
 use crate::state::{IslandState, LeaderboardEntry, TopImageEntry};
-use axum::extract::{State};
-use axum::response::{IntoResponse, Sse, sse::{Event as AxumSseEvent, KeepAlive}};
+use axum::extract::{Path, State};
+use axum::response::{
+    IntoResponse, Response, Sse,
+    sse::{Event as AxumSseEvent, KeepAlive},
+};
 use axum::{Form, Json};
 use base64::Engine;
 use base64::prelude::BASE64_STANDARD;
 use futures::Stream;
 use image::{GenericImageView, ImageReader, Pixel};
+use reqwest::Client;
 use serde::Deserialize;
+use std::fmt::Display;
 use std::io::Cursor;
 use tokio_stream::StreamExt;
 use tokio_stream::wrappers::BroadcastStream;
@@ -28,10 +33,7 @@ pub struct SubmitGrassForm {
 #[axum::debug_handler]
 pub async fn submit_grass(
     State(state): State<IslandState>,
-    Form(SubmitGrassForm {
-        name,
-        file,
-    }): Form<SubmitGrassForm>,
+    Form(SubmitGrassForm { name, file }): Form<SubmitGrassForm>,
 ) -> Result<impl IntoResponse, IslandError> {
     let uuid = state.start_submission().await?; //TODO: make this actually not just be useless lolll
 
@@ -49,23 +51,28 @@ pub async fn submit_grass(
         let [red, green, blue] = pixel.to_rgb().0;
 
         count += 1.0;
-        sum_distance += ((red as f32).powi(2) + (255.0 - green as f32).powi(2) + (blue as f32).powi(2)).sqrt();
+        sum_distance +=
+            ((red as f32).powi(2) + (255.0 - green as f32).powi(2) + (blue as f32).powi(2)).sqrt();
     }
 
-
-    const MAX_DISTANCE: f32 = 441.67295593; // 255.0 * 3.0_f32.sqrt()
+    const MAX_DISTANCE: f32 = 441.672_94; // 255.0 * 3.0_f32.sqrt()
     let average_distance = sum_distance / count;
-    let score = ((1.0 - average_distance / MAX_DISTANCE) * 100.0).max(0.0).min(100.0) as u32;
+    let score = ((1.0 - average_distance / MAX_DISTANCE) * 100.0)
+        .clamp(0.0, 100.0) as u32;
 
     info!(?average_distance, ?score);
 
     state.add_score(uuid, name.clone(), score).await?;
-    state.set_potential_top_image(name, score, file_contents).await?;
+    state
+        .set_potential_top_image(name, score, file_contents)
+        .await?;
 
     Ok("/")
 }
 
-pub async fn get_leaderboard(State(state): State<IslandState>) -> Result<Json<Vec<LeaderboardEntry>>, IslandError> {
+pub async fn get_leaderboard(
+    State(state): State<IslandState>,
+) -> Result<Json<Vec<LeaderboardEntry>>, IslandError> {
     Ok(Json(state.get_leaderboard().await?))
 }
 
@@ -84,6 +91,50 @@ pub async fn leaderboard_sse(
     Sse::new(stream).keep_alive(KeepAlive::default())
 }
 
-pub async fn top_images (State(state): State<IslandState>) -> Result<Json<Vec<TopImageEntry>>, IslandError> {
+pub async fn top_images(
+    State(state): State<IslandState>,
+) -> Result<Json<Vec<TopImageEntry>>, IslandError> {
     state.get_top_images().await.map(Json)
+}
+
+pub async fn get_page(
+    State(state): State<IslandState>,
+    path: Option<Path<String>>,
+) -> Result<impl IntoResponse, IslandError> {
+    Ok(match path {
+        Some(Path(path)) => get_with_path(state.client, path).await?.into_response(),
+        None => index(State(state)).await?.into_response(),
+    })
+}
+
+pub async fn get_asset(
+    State(state): State<IslandState>,
+    Path(asset): Path<String>,
+) -> Result<impl IntoResponse, IslandError> {
+    get_with_path(state.client, format!("Assets/{asset}")).await
+}
+
+pub async fn index(State(state): State<IslandState>) -> Result<impl IntoResponse, IslandError> {
+    get_with_path(state.client, "index.html").await
+}
+
+pub async fn get_with_path(
+    client: Client,
+    path: impl Display + AsRef<std::path::Path>,
+) -> Result<Response, IslandError> {
+    let path_path = path.as_ref();
+    let url = format!(
+        "https://raw.githubusercontent.com/BurntNail/TouchingGrass25/refs/heads/main/Html/{}",
+        path
+    );
+    let rsp = client.get(url).send().await?;
+
+    let mime = mime_guess::from_path(path_path)
+        .first()
+        .unwrap()
+        .to_string();
+    info!(?mime);
+    let bytes = rsp.bytes().await?.to_vec();
+
+    Ok(([(http::header::CONTENT_TYPE, mime)], bytes).into_response())
 }
--- theisland/src/state.rs
+++ theisland/src/state.rs
@@ -1,21 +1,22 @@
+use crate::errors::IslandError;
+use redis::aio::MultiplexedConnection;
+use redis::{AsyncTypedCommands, Client};
+use redis_macros::FromRedisValue;
+use s3::creds::Credentials;
+use s3::{Bucket, Region};
 use serde::{Deserialize, Serialize};
+use sha256::digest;
 use std::cmp::Reverse;
 use tokio::sync::broadcast::{Receiver, Sender, channel};
 use tracing::error;
 use uuid::Uuid;
-use redis::aio::MultiplexedConnection;
-use redis::{AsyncTypedCommands, Client};
-use redis_macros::{FromRedisValue};
-use s3::{Bucket, Region};
-use s3::creds::Credentials;
-use sha256::digest;
-use crate::errors::IslandError;
 
 #[derive(Clone)]
 pub struct IslandState {
     redis_connection: MultiplexedConnection,
     update_leaderboard: Sender<Vec<LeaderboardEntry>>,
-    bucket: Box<Bucket>
+    bucket: Box<Bucket>,
+    pub client: reqwest::Client,
 }
 
 #[derive(Serialize, Debug, Clone)]
@@ -35,19 +36,19 @@ struct InternalTopImage {
 pub struct TopImageEntry {
     pub person: String,
     pub image_score: u32,
-    pub image: String
+    pub image: String,
 }
 
 impl IslandState {
-    pub async fn new () -> Result<Self, IslandError> {
+    pub async fn new() -> Result<Self, IslandError> {
         let (update_leaderboard, _) = channel(1);
-
 
         let redis_connection = {
             let path = std::env::var("REDIS_PATH")?;
             let client = Client::open(path)?;
 
-            let (redis_connection, drive_future) = client.create_multiplexed_tokio_connection().await?;
+            let (redis_connection, drive_future) =
+                client.create_multiplexed_tokio_connection().await?;
             tokio::spawn(drive_future); //TODO: nice ending for ctrl-c
 
             redis_connection
@@ -60,20 +61,32 @@ impl IslandState {
             let region = std::env::var("AWS_REGION")?;
             let secret_access_key = std::env::var("AWS_SECRET_ACCESS_KEY")?;
 
-            let creds = Credentials::new(Some(&access_key_id), Some(&secret_access_key), None, None, None)?;
-            let region = Region::Custom {region, endpoint};
+            let creds = Credentials::new(
+                Some(&access_key_id),
+                Some(&secret_access_key),
+                None,
+                None,
+                None,
+            )?;
+            let region = Region::Custom { region, endpoint };
             Bucket::new(&bucket_name, region, creds)?
         };
 
         Ok(Self {
-            update_leaderboard, redis_connection, bucket
+            update_leaderboard,
+            redis_connection,
+            bucket,
+            client: reqwest::Client::new(),
         })
     }
 
     pub async fn start_submission(&self) -> Result<Uuid, IslandError> {
         let uuid = Uuid::new_v4();
 
-        self.redis_connection.clone().sadd("valid_uuids", uuid.to_string()).await?;
+        self.redis_connection
+            .clone()
+            .sadd("valid_uuids", uuid.to_string())
+            .await?;
 
         Ok(uuid)
     }
@@ -98,11 +111,14 @@ impl IslandState {
     pub async fn get_leaderboard(&self) -> Result<Vec<LeaderboardEntry>, IslandError> {
         let mut conn = self.redis_connection.clone();
 
-        let mut scores: Vec<LeaderboardEntry> = conn.hgetall("scores").await?
+        let mut scores: Vec<LeaderboardEntry> = conn
+            .hgetall("scores")
+            .await?
             .into_iter()
             .map(|(person, score)| {
                 Ok(LeaderboardEntry {
-                    person, score: score.parse()?
+                    person,
+                    score: score.parse()?,
                 })
             })
             .collect::<Result<_, IslandError>>()?;
@@ -115,44 +131,47 @@ impl IslandState {
         self.update_leaderboard.subscribe()
     }
 
-    async fn get_top_info (conn: &mut MultiplexedConnection) -> Result<Vec<InternalTopImage>, IslandError> {
+    async fn get_top_info(
+        conn: &mut MultiplexedConnection,
+    ) -> Result<Vec<InternalTopImage>, IslandError> {
         let Some(json) = conn.get("top_img_info").await? else {
             return Ok(vec![]);
         };
         Ok(serde_json::from_str(&json)?)
     }
 
-    pub async fn get_top_images (&self) -> Result<Vec<TopImageEntry>, IslandError> {
+    pub async fn get_top_images(&self) -> Result<Vec<TopImageEntry>, IslandError> {
         let mut redis_conn = self.redis_connection.clone();
 
         let current_top = Self::get_top_info(&mut redis_conn).await?;
 
         let mut ret = vec![];
         for info in current_top {
-            let image = self.bucket.presign_get(
-                info.s3_path,
-                60,
-                None
-            ).await?;
+            let image = self.bucket.presign_get(info.s3_path, 60, None).await?;
 
             ret.push(TopImageEntry {
                 person: info.person,
                 image_score: info.image_score,
-                image
+                image,
             });
         }
 
         Ok(ret)
     }
 
-    pub async fn set_potential_top_image (&self, person: String, image_score: u32, image: Vec<u8>) -> Result<(), IslandError> {
+    pub async fn set_potential_top_image(
+        &self,
+        person: String,
+        image_score: u32,
+        image: Vec<u8>,
+    ) -> Result<(), IslandError> {
         let mut redis_conn = self.redis_connection.clone();
 
         let mut current_top = Self::get_top_info(&mut redis_conn).await?;
 
         let mut index_to_insert = None;
-        if current_top.is_empty() {
-            index_to_insert = Some(0);
+        if current_top.len() < 3 {
+            index_to_insert = Some(current_top.len());
         } else {
             for (i, info) in current_top.iter().enumerate() {
                 if info.image_score < image_score {
@@ -169,11 +188,14 @@ impl IslandState {
         let s3_path = digest(&image).to_string();
         self.bucket.put_object(&s3_path, &image).await?;
 
-        current_top.insert(index_to_insert, InternalTopImage {
-            person,
-            image_score,
-            s3_path,
-        });
+        current_top.insert(
+            index_to_insert,
+            InternalTopImage {
+                person,
+                image_score,
+                s3_path,
+            },
+        );
         current_top.truncate(3);
 
         let jsoned_top = serde_json::to_string(&current_top)?;
