679b2897cb259ac3749db06be0a08bfa6ade4af4
Jack Maguire <jack@maguire.tech>
2025-10-25 17:37:51 +0100

    add SSE to comments

--- Html/comments.js
+++ Html/comments.js
@@ -1,27 +1,36 @@
+function updateComments (json) {
+    const chatContainer = document.querySelector(".chat-messages");
+    chatContainer.innerHTML = "";
+
+    for (let comment of json) {
+        const wholeCommentContainer = document.createElement("div");
+        const innerDiv = document.createElement("div");
+        wholeCommentContainer.classList.add("comment");
+        innerDiv.classList.add("comment-content");
+
+        const username = document.createElement("span");
+        const text = document.createElement("span");
+        username.classList.add("username");
+        text.classList.add("text");
+
+        username.innerText = comment.name;
+        text.innerText = comment.content;
+
+        innerDiv.appendChild(username);
+        innerDiv.appendChild(text);
+        wholeCommentContainer.appendChild(innerDiv);
+        chatContainer.appendChild(wholeCommentContainer);
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     fetch('https://theisland.fly.dev/all_comments').then(rsp => {
-        rsp.json().then(json => {
-            const chatContainer = document.querySelector(".chat-messages");
-
-            for (let comment of json) {
-                const wholeCommentContainer = document.createElement("div");
-                const innerDiv = document.createElement("div");
-                wholeCommentContainer.classList.add("comment");
-                innerDiv.classList.add("comment-content");
-
-                const username = document.createElement("span");
-                const text = document.createElement("span");
-                username.classList.add("username");
-                text.classList.add("text");
-
-                username.innerText = comment.name;
-                text.innerText = comment.content;
-
-                innerDiv.appendChild(username);
-                innerDiv.appendChild(text);
-                wholeCommentContainer.appendChild(innerDiv);
-                chatContainer.appendChild(wholeCommentContainer);
-            }
-        });
+        rsp.json().then(updateComments);
     });
+
+    const sse = new EventSource("https://theisland.fly.dev/comments_sse");
+    sse.onmessage = (event) => {
+        let json = JSON.parse(event.data);
+        updateComments(json);
+    };
 });
