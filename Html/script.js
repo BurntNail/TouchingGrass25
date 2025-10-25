855f0024905739e9a774673907056ab737ac814d
michalina07 <michalianadymitruk@gmai.com>
2025-10-25 12:00:14 +0100

    update table

--- Html/script.js
+++ Html/script.js
@@ -1,9 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const table = document.querySelector("table");
-    const from_web = fetch('https://theisland.fly.dev/leaderboard');
+    const url = 'https://theisland.fly.dev/leaderboard';
+    const table = document.querySelector("tbody");
+    //fetches resources from the web
+
+    const from_web = fetch(url);
+    //updates the leaderboard, response as a string
     from_web.then(response => {
-        response.text().then(data => {
-            table.innerHTML = data;
+        response.json().then(data => {
+            for (let leaderboardEntry of data) {
+                let row = document.createElement("tr");
+
+                let nameElement = document.createElement("td");
+                nameElement.innerText = leaderboardEntry.person;
+                row.appendChild(nameElement);
+
+
+                table.appendChild(row);
+            }
         });
     });
 });
