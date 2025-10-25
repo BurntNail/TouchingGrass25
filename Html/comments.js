91a3abbda0191ba02589f50daad0bb2fd04f064f
michalina07 <michalianadymitruk@gmai.com>
2025-10-25 16:32:12 +0100

    v1 of forums js

---
+++ Html/comments.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('https://theisland.fly.dev/all_comments').then(rsp => {
+        rsp.json().then(json => {
+            const chatContainer = document.querySelector(".chat-messages");
+
+            for (let comment of json) {
+                const wholeCommentContainer = document.createElement("div");
+                const innerDiv = document.createElement("div");
+                wholeCommentContainer.classList.add("comment");
+                innerDiv.classList.add("comment-content");
+
+                const username = document.createElement("span");
+                const text = document.createElement("span");
+                username.innerText = comment.name;
+                text.innerText = comment.content;
+
+                innerDiv.appendChild(username);
+                innerDiv.appendChild(text);
+                wholeCommentContainer.appendChild(innerDiv);
+                chatContainer.appendChild(chatContainer);
+            }
+        });
+    });
+});
